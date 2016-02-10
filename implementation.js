2c0f397ebf0e70e383489f293d4c75b6b42f8dc9
Tjen Wellens <tjen.wellens@gmail.com>
2016-02-10 10:18:47 +0100

    simple message returning city (no weather)

--- implementation.js
+++ implementation.js
@@ -2,6 +2,7 @@
 var request = require('request');
 
 exports.getWeather = function(args, done){
+	console.log("getWeather: " + args);
 	let url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + args.city + 
 					'&mode=json&units=metric&cnt=16&APPID=c96d6e485f7907e490a8b3b1aa98b502';
 	request.get(url, function(err, response){
@@ -9,6 +10,7 @@ exports.getWeather = function(args, done){
 			console.error(err);
 			return done(err, null);
 		}
-		return done(null, response);
+		//return done(null, response);
+		return done(null, "<div>Toda's weather for "+args.city+"</div>");
 	});
 };
